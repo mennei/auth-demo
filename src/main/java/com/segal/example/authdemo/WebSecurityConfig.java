00a896bb6665a8133ee5f31d18870993f09ee8f1
mennei <mennei.segal@gmail.com>
2020-01-26 12:09:56 +0200

    Simple app to demo authentication with spring-boot

---
+++ src/main/java/com/segal/example/authdemo/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.segal.example.authdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        /*
+        withDefaultPasswordEncoder()
+        Deprecated.
+        Using this method is not considered safe for production, but is acceptable for demos and getting started.
+        For production purposes, ensure the password is encoded externally. See the method Javadoc for additional
+        details.
+        There are no plans to remove this support. It is deprecated to indicate that this is considered insecure for
+        production purposes.
+         */
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
